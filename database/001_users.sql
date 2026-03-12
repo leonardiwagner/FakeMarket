06f0dc982f8699356946723f9ab9da040b5473f1
Wagner Leonardi <leonardiwagner@gmail.com>
2026-03-12 19:40:15 +0100

    update schema

--- database/001_users.sql
+++ database/001_users.sql
@@ -1,3 +1,8 @@
+-- reset database
+drop schema public cascade;
+create schema public;
+
+-- users
 create table users (
   id uuid primary key default gen_random_uuid(),
   email text not null,
@@ -12,52 +17,82 @@ create table users (
 
 create index users_type_idx on users(type);
 
-INSERT INTO public.users(email, password, type) VALUES ('admin@fakemarket.com', 'pass123', 'admin');
-INSERT INTO public.users(email, password, type) VALUES ('samara@fakemarket.com', 'pass123', 'robot');
-INSERT INTO public.users(email, password, type) VALUES ('patrick@fakemarket.com', 'pass123', 'robot');
-INSERT INTO public.users(email, password, type) VALUES ('marta@fakemarket.com', 'pass123', 'robot');
-INSERT INTO public.users(email, password, type) VALUES ('james@fakemarket.com', 'pass123', 'robot');
-INSERT INTO public.users(email, password, type) VALUES ('philip@fakemarket.com', 'pass123', 'robot');
+insert into public.users (id, email, password, type) values
+  ('11111111-1111-1111-1111-111111111111', 'admin@fakemarket.com',   'pass123', 'admin'),
+  ('22222222-2222-2222-2222-222222222222', 'samara@fakemarket.com',  'pass123', 'robot'),
+  ('33333333-3333-3333-3333-333333333333', 'patrick@fakemarket.com', 'pass123', 'robot'),
+  ('44444444-4444-4444-4444-444444444444', 'marta@fakemarket.com',   'pass123', 'robot'),
+  ('55555555-5555-5555-5555-555555555555', 'james@fakemarket.com',   'pass123', 'robot'),
+  ('66666666-6666-6666-6666-666666666666', 'kevin@fakemarket.com',   'pass123', 'robot');
 
+-- resources
 create table resources (
   id uuid primary key default gen_random_uuid(),
-  name text not null,  
-  
+  name text not null,
+
   constraint resources_name_uq unique (name)
 );
 
-INSERT INTO public.resources(name) VALUES ('oil');
-INSERT INTO public.resources(name) VALUES ('gold');
+insert into public.resources (id, name) values
+  ('aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'oil'),
+  ('bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb', 'gold');
 
+-- orders
 create table orders (
   id uuid primary key default gen_random_uuid(),
   user_id uuid not null references users(id) on delete restrict,
   resource_id uuid not null references resources(id) on delete restrict,
-  
+
   type text not null,
-  status text not null default 'PENDING',
+  status text not null default 'pending',
   quantity integer not null,
-  price numeric(20,4) not null,
+  price numeric(20,2) not null,
   has_price_limit boolean not null default true,
 
   created timestamptz not null default now(),
   processed timestamptz
 );
 
-insert into public.orders (user_id, resource_id, type, quantity, price)
-values (
-  (select id from public.users where email = 'samara@fakemarket.com'),
-  (select id from public.resources where name = 'oil'),
-  'sell',
-  1000,
-  10.5
+-- initial seed of resources via sell orders
+insert into public.orders (
+  id, user_id, resource_id, type, status, quantity, price
+) values
+  ('10000000-0000-0000-0000-000000000001', '11111111-1111-1111-1111-111111111111', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'sell', 'executed', 1000, 10.15),
+  ('10000000-0000-0000-0000-000000000002', '11111111-1111-1111-1111-111111111111', 'bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb', 'sell', 'executed', 1000, 15.15);
+
+-- initial seed of buy orders to create some trades and populate the trade history
+insert into public.orders (
+  id, user_id, resource_id, type, status, quantity, price
+) values
+  ('20000000-0000-0000-0000-000000000001', '22222222-2222-2222-2222-222222222222', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'buy',  'executed', 100, 10.5),
+  ('20000000-0000-0000-0000-000000000002', '33333333-3333-3333-3333-333333333333', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'buy',  'executed', 100, 10.5),
+  ('20000000-0000-0000-0000-000000000003', '44444444-4444-4444-4444-444444444444', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'buy',  'executed', 100, 10.5),
+  ('20000000-0000-0000-0000-000000000004', '55555555-5555-5555-5555-555555555555', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'buy',  'executed', 100, 10.5),
+  ('20000000-0000-0000-0000-000000000005', '66666666-6666-6666-6666-666666666666', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'buy',  'executed', 100, 10.5);
+
+-- trades
+create table trades (
+  id uuid primary key default gen_random_uuid(),
+
+  buy_order_id uuid not null references orders(id) on delete restrict,
+  sell_order_id uuid not null references orders(id) on delete restrict,
+
+  resource_id uuid not null references resources(id) on delete restrict,
+
+  quantity integer not null,
+  price numeric(20,2) not null,
+
+  created timestamptz not null default now()
 );
 
-insert into public.orders (user_id, resource_id, type, quantity, price)
-values (
-  (select id from public.users where email = 'patrick@fakemarket.com'),
-  (select id from public.resources where name = 'gold'),
-  'sell',
-  500,
-  15.1
- );
+create index trades_resource_id_executed_at_idx on trades(resource_id, created desc);
+
+-- initial trades to determine the initial market price of the resources
+insert into public.trades (
+  id, sell_order_id, buy_order_id, resource_id, quantity, price
+) values
+  ('30000000-0000-0000-0000-000000000001', '10000000-0000-0000-0000-000000000001', '20000000-0000-0000-0000-000000000001', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 100, 10.5),
+  ('30000000-0000-0000-0000-000000000002', '10000000-0000-0000-0000-000000000001', '20000000-0000-0000-0000-000000000002', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 100, 10.5),
+  ('30000000-0000-0000-0000-000000000003', '10000000-0000-0000-0000-000000000001', '20000000-0000-0000-0000-000000000003', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 100, 10.5),
+  ('30000000-0000-0000-0000-000000000004', '10000000-0000-0000-0000-000000000001', '20000000-0000-0000-0000-000000000004', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 100, 10.5),
+  ('30000000-0000-0000-0000-000000000005', '10000000-0000-0000-0000-000000000001', '20000000-0000-0000-0000-000000000005', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 100, 10.5);
--- robots/src/orderGenerator.ts
+++ robots/src/orderGenerator.ts
@@ -1,7 +1,18 @@
 import { User,UserType,getUsers, getUsersByType} from './db'
 
 async function generateOrders() {
+    // TODO get the robot users and generate orders for them
     const robotUsers = getUsersByType(UserType.ROBOT);
+
+    // TODO get robot resources amount
+
+    // TODO get the latest market price of the resources
+
+    // TODO get recent trades to determine the price trend
+
+    // TODO check current open sell prices to determine if we should place buy orders or sell orders
+
+
 }
 
 
